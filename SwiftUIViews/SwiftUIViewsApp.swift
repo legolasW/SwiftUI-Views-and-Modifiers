e3bbf531c716478b8247cea098d8e1b7013f25b1
Legolas <057@live.com>
2020-11-14 20:16:44 -0800

    Initial Commit

---
+++ SwiftUIViews/SwiftUIViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewsApp.swift
+//  SwiftUIViews
+//
+//  Created by Legolas on 11/14/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
