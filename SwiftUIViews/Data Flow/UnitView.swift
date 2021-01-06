d9ef66da6b86cb08f9592a3b6f6bee94b53f2844
Legolas <057@live.com>
2021-01-06 14:43:39 -0800

    Added property wrapper.

---
+++ SwiftUIViews/Data Flow/UnitView.swift
@@ -0,0 +1,28 @@
+//
+//  UnitView.swift
+//  SwiftUIViews
+//
+//  Created by Legolas on 2021-01-06.
+//
+
+import SwiftUI
+
+struct UnitView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+
+
+
+
+
+
+
+
+struct UnitView_Previews: PreviewProvider {
+    static var previews: some View {
+        UnitView()
+    }
+}
---
+++ SwiftUIViews/Data Flow/ViewOne.swift
@@ -0,0 +1,56 @@
+//
+//  StateAndBinding.swift
+//  SwiftUIViews
+//
+//  Created by Legolas on 2021-01-04.
+//
+
+import SwiftUI
+
+
+
+
+
+
+
+struct ViewOne: View {
+    @State var userInput = ""
+    
+    var body: some View {
+        ViewTwo(userInput: $userInput)
+    }
+}
+
+struct ViewTwo: View {
+    @Binding var userInput: String
+    
+    var body: some View {
+        TextField("输入用户名", text: $userInput)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct StateAndBinding_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewOne()
+    }
+}
+
+
+
+
